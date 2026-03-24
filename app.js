c455b3dbd01b3e2c7c4b3a3d0aff800dc1abec3d
ooopHHH <pedrohgp2606@hotmail.com>
2026-03-23 21:14:39 -0300

    app.js unnecessary code removed as it was only for test purposes

--- app.js
+++ app.js
@@ -3,16 +3,8 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const pool = require('./db');
 
 require('./loaders/express')(app);
-
-async function usersList() {
-  const allUsers = await pool.query('SELECT * FROM users')
-  return(allUsers.rows)
-}
-
-usersList()
 
 app.listen(3000, () => {
   console.log(`Listening on http://localhost:3000`)
